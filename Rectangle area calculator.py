1429c8fabb5b634329818353e9d9530899b3cc4a
Aditi Rathore <aditi.rathore2021@gmail.com>
2024-11-03 18:15:36 +0530

    Add files via upload

---
+++ Rectangle area calculator.py
@@ -0,0 +1,5 @@
+def cal () :
+    width=int(input("enter the width of a rectangle:"))
+    height=int(input("enter the height of a rectangle:"))
+    area=width*height
+    print("the area of the rectangle is",area)
